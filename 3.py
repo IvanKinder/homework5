b1e13d4ed1f18985c567b719a64ac713fea30615
Ivan Kinder <ivankinder11@gmail.com>
2020-04-01 15:36:58 +0300

    Небольшой апдейт третьего задания

--- 3.py
+++ 3.py
@@ -1,6 +1,7 @@
 with open("costs.txt") as file_1:
     people = file_1.read().split()
     my_dict = {}
+    j = 1
 
     for i in range(0, len(people) - 1, 2):
         my_dict[people[i]] = float(people[i + 1])
@@ -8,7 +9,8 @@ with open("costs.txt") as file_1:
     print("Сотрудники, имеющие заработную плату менее 20000: ")
     for key, value in my_dict.items():
         if value < 20000:
-            print(key)
+            print(f"{j}. {key}")
+            j += 1
         s = sum((my_dict.values()))
         people_count = len(my_dict.values())
     print(f"Средний доход сотрудников: {s / people_count :.2f}")
